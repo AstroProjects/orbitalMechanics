9f8adda61e0e1083e54514abb80869153b1867de
Eva Maria Urbano <eva1616@gmail.com>
2018-01-20 00:42:41 +0100

    r=(x,y,z) corrections...

--- code/HeliocentricOrbitalElements/Main_HeliocentricOrbitalElements.m
+++ code/HeliocentricOrbitalElements/Main_HeliocentricOrbitalElements.m
@@ -14,9 +14,16 @@ eliptic=true;
 
 %Calcul inclinació i (named 'in' because 'i' is usually used when doing
 %iterations) (equacions pag. 14 tema5b)
-dLamb=atan(rM_t2(2)/rM_t2(1))-atan(rT_t1(2)/rT_t1(1));
+lambda1=atan(rT_t1(2)/rT_t1(1));
+lambda2=atan(rM_t2(2)/rM_t2(1));
+dLamb=lambda2-lambda1;
+beta1=asin(rT_t1(3)/norm(rT_t1));
 beta2=asin(rM_t2(3)/norm(rM_t2));
 inc=atand(tan(beta2)/sin(dLamb)); %[º]
+
+%r1 & r2:
+r1=(rT_t1(1)/(cos(beta1)*cos(lambda1)));
+r2=(rM_t2(1)/(cos(beta2)*cos(lambda2)));
 
 %Calcul increment the theta (equacions pag. 14 tema5b)
 dTheta=acosd(cos(dLamb)*cos(beta2)); %[º]
@@ -27,14 +34,14 @@ Omega=atand(rT_t1(2)/rT_t1(1)); %[º]
 if eliptic == true
     disp('Computing eliptic trajectory...');
     %It has been considered that the equations work in degrees, but it is not sure...
-    [e, a, theta1]=Computeeliptic(rT_t1(1),rM_t2(1),dt,dTheta);
+    [e, a, theta1]=Computeeliptic(r1,r2,dt,dTheta);
     if theta1>=360.001
         disp('No theta possible');
     end
 else
     disp('Computing hyperbolic trajectory...');
     %It has been considered that the equations work in degrees, but it is not sure...
-    [e, a, theta1]=Computehyperbolic(rT_t1(1),rM_t2(1),dt,dTheta);
+    [e, a, theta1]=Computehyperbolic(r1,r2,dt,dTheta);
     if theta1>=360.001
         disp('No theta possible');
     end
