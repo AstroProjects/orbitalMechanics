2e3afeed22721751c558d58ef447617a71d8f23e
Eva Maria Urbano <eva1616@gmail.com>
2018-01-20 14:11:15 +0100

    Eliptic from Earth working

--- code/HeliocentricOrbitalElements/Computeeliptic.m
+++ code/HeliocentricOrbitalElements/Computeeliptic.m
@@ -3,7 +3,7 @@ function [e, a, theta1]=Computeeliptic(r1,r2,dt,dThet)
 %three equations. Theory can be seen in page 23-25 of Tema5b.
 theta1=0; %[º]
 error=100;
-while (error>10) && (theta1<=360.001)
+while (error>0.001) && (theta1<=360.001)
         theta1=theta1+0.001;
         %Eccentricity calculation. Its value has to be between 0 and 1
         e=(r2-r1)/(r1*cosd(theta1)-r2*cosd(theta1+dThet));
@@ -11,9 +11,9 @@ while (error>10) && (theta1<=360.001)
             % Semieix
             a=(r1*(1+e*cosd(theta1)))/(1-e^2);
             %Temps de viatge
-            t2t1=(365.25/(2*pi))*(a^(3/2))*(2*atand(sqrt((1-e)/(1+e))*tand((theta1+dThet)/2))...
+            t2t1=(365.25/(2*pi))*(a^(3/2))*(2*atan(sqrt((1-e)/(1+e))*tand((theta1+dThet)/2))...
                 -((e*sqrt(1-e^2)*sind(theta1+dThet))/(1+e*cosd(theta1+dThet)))...
-                -2*atand(sqrt((1-e)/(1+e))*tand(theta1/2))+(e*sqrt(1-e^2)*sind(theta1))/(1+e*cosd(theta1)));
+                -2*atan(sqrt((1-e)/(1+e))*tand(theta1/2))+(e*sqrt(1-e^2)*sind(theta1))/(1+e*cosd(theta1)));
             error=abs(t2t1-dt);
         else
             error=1000;
