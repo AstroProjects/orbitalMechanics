b1fe1f6e0ed0fbb8d95ac7baade23bf9d81526cb
Pol Fontanes <pol.f.48@gmail.com>
2018-01-20 12:17:41 +0100

    Pol code

--- code/GeneralManouvers/EarthMars1.m
+++ code/GeneralManouvers/EarthMars1.m
@@ -9,3 +9,28 @@ dThet0 = 141.683;%[º]
 
 rT_t1 = [0.4537, -0.9094, 0.0000]; %[AU]
 rM_t2 = [0.3148, 1.5078, 0.0239]; %[AU]
+
+%%
+R = 384400; %[km]
+G = 6.67428e-11; %[m^3·kg^-1·s^-2]
+
+%SpaceCraft (SC)
+SC.r0 = 300; %[km]
+SC.t0 = 0;
+SC.v0 = 10.84; %[km/s]
+SC.lamb = 37.5; %[º]
+
+%Earth
+DP.R = 6371; %[km]
+DP.M = 5.972e24; %[kg]
+DP.mu = DP.M*G/1000^3; %[km^3/s^2]
+%Mars
+AP.Re = 66183; %[km]
+AP.R = AP.Re/38; %[km]
+%Sun
+Sun.R = 6.960e8; %[m]
+Sun.M = 1.989e30; %[kg]
+Sun.mu = Sun.M*G/1000^3; %[km^3/s^2]
+%Orbital parameters
+O.R_dp = 149.6e6; %[km] Orbital radii of the departure planet
+O.R_ap = 227.9e6; %[km] Orbital radii of the arrival planet
--- code/GeneralManouvers/Main_GeneralManoeuvres.m
+++ code/GeneralManouvers/Main_GeneralManoeuvres.m
@@ -1,6 +1,6 @@
 clc; clear; close all;
 %% GENERAL MANOEUVRES
-%{ 
+%{
 We want to find: Omega, i, omega, a, e theta 1
 Methode imposes: t1, t2
 %}
@@ -9,3 +9,17 @@ Methode imposes: t1, t2
 NAME_INPUT_DATA = 'EarthMars1' ;
 % Load data files
 eval(NAME_INPUT_DATA);
+
+%% PLANETARY DEPARTURE
+%hyperbolic excess speed
+v_inf = sqrt(Sun.mu/O.R_dp)*(sqrt((2*O.R_ap)/(O.R_dp + O.R_ap))-1); %[km/s]
+
+%Space craft speed on circular parking orbit
+vc = sqrt(DP.mu/(DP.R + SC.r0)); %[km/s]
+
+%delta-v to step up to the departue hyperbola
+delta_v = vc*(sqrt(2+(v_inf/vc)^2)-1); %[km/s]
+
+%perigee of departure hyperbola
+rp = DP.R + SC.r0;
+beta = acosd(1/(1+(rp*v_inf^2/DP.mu))); %[º]
