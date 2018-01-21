97b4f5d47c6438e08511153f25302f94879166cf
Pol Fontanes <pol.f.48@gmail.com>
2018-01-21 18:59:25 +0100

    Verification calculated

--- code/HeliocentricOrbitalElements - check/cases/EarthMars_hypC9.m
+++ code/HeliocentricOrbitalElements - check/cases/EarthMars_hypC9.m
@@ -1,7 +1,7 @@
 %% INTERPLANETARY DATA
 %Departure, Arrival and delta t
-t1 = [15, 1, 2022]; %day, month, year
-t2 = [20, 4, 2022]; %day, month, year
+t1 = [26, 11, 2021]; %day, month, year
+t2 = [19, 2, 2022]; %day, month, year
 dt = 95; %days
 
 rT_t1 = [-0.4079, 0.8950, 0.0000]; %[AU]
--- code/HeliocentricOrbitalElements - check/cases/MarsEarthC7.m
+++ code/HeliocentricOrbitalElements - check/cases/MarsEarthC7.m
@@ -1,12 +1,11 @@
 %% INTERPLANETARY DATA
 %Departure, Arrival and delta t
-t1 = [18, 7, 2025]; %day, month, year
-t2 = [21, 10, 2025]; %day, month, year
-dt = 95; %days
-
-%dLamb0 = 29.837; %[º]
-%dThet0 = 141.683;%[º]
+t1 = [20, 11, 2030]; %day, month, year
+t2 = [6, 7, 2031]; %day, month, year
+dt = 228; %days
 
 
-rT_t1 = [0.4342, 0.9188, 0.0001]; %[AU]
-rM_t2 = [0.6775, 1.3571, 0.0118]; %[AU]
+rT_t1 = [1.4166, 0.8722, 0.0530]; %[AU]
+rM_t2 = [0.2345, 0.9893, 0.0001]; %[AU]
+
+muS=1.32712440018e20;
